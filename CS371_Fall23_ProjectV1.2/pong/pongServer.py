dd7fa56f9be1a11abcee1f68f8de7647a271f2fc
Victor Lopez <67474092+Pikachou32@users.noreply.github.com>
2023-11-13 10:51:46 -0500

    All having the same idea of server so makes it defualt for all

--- CS371_Fall23_ProjectV1.2/pong/pongServer.py
+++ CS371_Fall23_ProjectV1.2/pong/pongServer.py
@@ -10,70 +10,42 @@
 import socket                                                  # Library needed for sockets
 import threading                                               # Library for threading
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # Creating the server
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    # Working on localhost need this
-server.bind(("localhost", 12321))                               # Binds the sockets to host and port 
-server.listen(2)                                                # listen to request of server from closer
+def threading_client(clientSocket, clientAddress):
+    print(f"Connected to {clientAddress}")
 
-
-class Network:                                                  # where we can connect to server and manage data around
-    def __init__(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    # Working on localhost need this
-        self.server = "localhost"
-        self.port = 12321
-        self.address = (self.server, self.port)
-        self.position = self.connect()
-
-    def getposition(self):                                      # to get posititon
-        return self.position
-    
-    def connect(self):                                          # 
-        try: 
-            self.client.connect(self)
-            return self.client.recv(1024).decode()
-        except:
-            pass
-    def send(self,data):
+    while True:
         try:
-            self.client.send(str.encode(data))
-            return self.cleint.recv(1024).decode()
-        except socket.error as e:
-            print(e)
+            data = clientSocket.recv(1024)
+            if not data:
+                break                                         # Exit  loop if no data is received from client
+
+            decoded_data = data.decode('utf-8')
+            print(f"Received from {clientAddress}: {decoded_data}")
+            clientSocket.sendall(data)
+        except Exception as e:
+            print(f"Error: {e}")
+            break   
             
 
 
 
-def threading_client(clientSocket, player):                     # threading funnction to communicate with client
-    msg = ''
-    while True:
-        try:
-            data = clientSocket.recv(1024)                      # Infromation trying to receive 
-            msg = data.decode('utf-8')                          # We need to decode infromation from utf-8 format
-
-            if not data:                                        # if information from client is not working, we disconect
-                print(f"Disconnected from player {player}")                           
-                break
-            else:                                                
-                print(f"Received from player {player}: {msg}")
-                clientSocket.send(data)                         # echo data  if data was received from client 
-        except:                                                 # breaks if running to errors to avouid infiinte loop
-            break
-    print("Connection closed")
-    clientSocket.close()
 
 
 
 def main():                                                     # main function for the server 
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # Creating the server
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)    # Working on localhost need this
+    server.bind(("localhost", 12321))                               # Binds the sockets to host and port 
+    server.listen(2)                                                # listen to request of server from closer
 
-    player = 0                                                  # creating player varaible 
+
     while True:                                         
         print("Waiting for connection ...")
         (clientSocket, clientAddress) = server.accept()
         print(f"Connection from {clientAddress}")
-        client_thread = threading.Thread(target= threading_client, args= (clientSocket,player))
-        player += 1
+        client_thread = threading.Thread(target= threading_client, args= (clientSocket,clientAddress))
         client_thread.start()
+
 
 if __name__ == "__main__":
     main()
