e9050bed88ace1e3e6a0fa0f4dff95a12e57bb1b
claydav25 <149725580+claydav25@users.noreply.github.com>
2023-11-16 22:02:10 -0500

    Update pongServerCD.py
    added server_state to help maintain updates across each client

--- CS371_Fall23_ProjectV1.2/pong/pongServerCD.py
+++ CS371_Fall23_ProjectV1.2/pong/pongServerCD.py
@@ -1,4 +1,4 @@
-# =================================================================================================
+#=======================================================================================
 # Contributing Authors:	    <Anyone who touched the code>
 # Email Addresses:          <Your uky.edu email addresses>
 # Date:                     <The date the file was last edited>
@@ -7,15 +7,14 @@
 # =================================================================================================
 
 import socket
-import threading
 import pickle
+import threading
 
 #define these ahead of time?
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-messages = [0, 0]
 lock = threading.Lock()
-
+BUFFER_SIZE = 1024
 # Use this file to write your server logic
 # You will need to support at least two clients
 # You will need to keep track of where on the screen (x,y coordinates) each paddle is, the score 
@@ -23,59 +22,104 @@ lock = threading.Lock()
 # I suggest you use the sync variable in pongClient.py to determine how out of sync your two
 # clients are and take actions to resync the games
 
-# Communication protocol for client-server
-def clientHandler(clientOneSocket, clientTwoSocket):
-    try:
-        while (True):
-                clientOneGameState = pickle.loads(clientOneSocket.recv(1024))
-                clientTwoGameState = pickle.loads(clientTwoSocket.recv(1024))
+# Declare the default server state
+server_sync = 0
+server_leftPaddle = 0
+server_rightPaddle = 0
+server_ballX = 55
+server_ballY = 55
+server_lScore = 0
+server_rScore = 0
+
+def clientHandler(clientSocket, player, other_client):
+    # choosing sides
+    if player == 0:
+        side = "left"
+        paddle = "left"
+    else:
+        side = "right"
+        paddle = "right"
+
+    # global variables to be updated with each client connection
+    global server_sync, server_leftPaddle, server_rightPaddle, server_ballX, server_ballY, server_lScore, server_rScore
+
+    server_currentPaddle = 55
+
+    # sending information to client
+    setup_info = {'screen_width': SCREEN_WIDTH, 'screen_height': SCREEN_HEIGHT, 'player_side': side}
+    clientSocket.sendall(pickle.dumps(setup_info))
+    
+    while True:
+        try:
             
-                # Thread lock to prevent sync updates during calculation
-                with lock:
-                
-                    # Variable to hold each sync variable to determine how out of sync
-                    clientOneSync = clientOneGameState['sync']
-                    clientTwoSync = clientTwoGameState['sync']
-                
+            with lock:
+                data = clientSocket.recv(BUFFER_SIZE)
+                game_state = pickle.loads(data)
 
-                    # Determine which game state is sent back to each client
-                    if (clientOneSync < clientTwoSync):
-                        gameState = pickle.dumps(clientTwoGameState)
-                        clientOneSocket.send(gameState)
+                if (game_state['sync'] > server_sync):
+                    server_sync = game_state['sync']
+                    server_currentPaddle = game_state['player_paddle']
+                    server_ballX = game_state['ballX']
+                    server_ballY = game_state['ballY']
+                    server_lScore = game_state['l_score']
+                    server_rScore = game_state['r_score']
+
+                    # Determine which paddle is the current paddle
+                    if (paddle == "left"):
+                        server_leftPaddle = server_currentPaddle
                     else:
-                        gameState = pickle.dumps(clientOneGameState)
-                        clientTwoSocket.send(gameState)
-
-    finally:
-        server.close()
+                        server_rightPaddle = server_currentPaddle
 
 
+                if not data:
+                    print(f"Disconnected from player: {[player]}")
+                    break
+                else:
+                    print(f"received from player {player}: {game_state}")
+                    server_state = {
+                        'sync': server_sync,
+                        'left_paddle': server_leftPaddle,
+                        'right_paddle': server_rightPaddle,
+                        'ballX': server_ballX,
+                        'ballY': server_ballY,
+                        'l_score': server_lScore,
+                        'r_score': server_rScore
+                    }
 
-if __name__ == "__main__":
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create server
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                    gameUpdate = pickle.dumps(server_state)
+                    clientSocket.send(gameUpdate)
+
+
+                # Update the other client with the received game state
+                if other_client is not None:
+                    other_client.sendall(pickle.dumps(game_state))
+
+        except Exception as e:
+            print(f"Error in player {player}: {e}")
+            break
+
+            
+
+
+if __name__ == "__main__":    
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)          # create server
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)        #allows us to use "Local host"
     server.bind(("localhost", 12321))
+    server.listen(5)  # listen for 5 concurrent connection attempts
+
     print("Awaiting connection...")
-    server.listen(5) #listen for 5 concurrent connection attempts
-
-    #accept two connections
-    clientOneSocket, clientOneAddress = server.accept()
-    clientTwoSocket, clientTwoAddress = server.accept()
-    print("Both clients have connected, running initial setup")
-
-
-    #assign sides, determine screen size
-    msg = {'screen_width': SCREEN_WIDTH, 'screen_height': SCREEN_HEIGHT, 'player_side': 'left'}
-    message = pickle.dumps(msg)
-    clientOneSocket.send(message)
-
-    msgTwo = {'screen_width': SCREEN_WIDTH, 'screen_height': SCREEN_HEIGHT, 'player_side': 'right'}
-    messageTwo = pickle.dumps(msgTwo)
-    clientTwoSocket.send(messageTwo)
-
-    print("Initial setup complete, starting game loop")
-        #receive data first
-    thread1 = threading.Thread(target=clientHandler, args=(clientOneSocket, clientTwoSocket))
-    thread2 = threading.Thread(target=clientHandler, args=(clientTwoSocket, clientOneSocket))
-    thread1.start()
-    thread2.start()
+    player = 0
+    client_sockets = [None, None]
+    with lock:
+        clientSocket, clientAddress = server.accept()
+        print(f"connected to: {clientAddress}")
+        # Store the client socket in the list
+        client_sockets[player] = clientSocket
+        client_thread = threading.Thread(target=clientHandler, args=(clientSocket,player, client_sockets[1- player]) )
+        player += 1
+    with lock:
+        clientSocket, clientAddress = server.accept()
+        print(f"connected to: {clientAddress}")
+        client_thread2 = threading.Thread(target=clientHandler, args=(clientSocket, player, client_sockets[1 - player]))
+        client_thread.start()
+        client_thread2.start()
